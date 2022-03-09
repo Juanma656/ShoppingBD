a7aa7db40a3047ba07ed14fc73adcf126e245829
JUAN MANUEL AGUDELO OLARTE <juanma112830@gmail.com>
2022-03-08 19:17:14 -0500

    Creation of data base and entity country with index

---
+++ ShoppingBD/ShoppingBD/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingBD.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El capo {0} es obligatorio.")]
+        
+        
+        public string Name { get; set; }
+    }
+}
